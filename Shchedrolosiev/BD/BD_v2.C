2373b12cd2c02a17d8b75730f143466a5758558e
shedprog <flynik75@gmial.com>
2019-02-08 12:08:31 +0200

    loolz

--- Shchedrolosiev/BD/BD_v2.C
+++ Shchedrolosiev/BD/BD_v2.C
@@ -20,7 +20,19 @@ int main(int argc, char** argv){
             array[i][j] = 0; 
     }
 
+    // initial point
+    array[0][0] = 1; array[1][0] = 1;
+    array[0][1] = 0; array[1][1] = 1;
+    for(int j = 1; i < N; j++){
+        for(int i = 0; i<2; i++)
+            array[j][i] = 1;
+    }
 
+
+    for(int i = 3; i < N; i++){
+        for(int j = min_h(); j < N; j++){
+            
+        }}
 
     for (int i = 0; i < N; i++){
           delete [] array[i];
