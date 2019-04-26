6bda25db323ad1b039856125b262af810a793174
shedprog <flynik75@gmial.com>
2019-04-26 11:06:37 +0300

    fix C++98

--- Shchedrolosiev/Q/Q.cpp
+++ Shchedrolosiev/Q/Q.cpp
@@ -10,8 +10,14 @@ int hesh(std::vector<char> v)
 	C_v.push_back(1);
 	long int sum = 0;
 	long int C;
-	for(char Ci : v){
-		std::cout<<0+Ci<<"\n";
+//	for(char Ci : v){
+//		std::cout<<0+Ci<<"\n";
+//		C = C_v[C_v.size() - 1];
+//		sum = (sum + (C * Ci)%D)%D;
+//		C_v.push_back( C * 1009 % D );
+//	}
+	for(int i=0;i<v.size();++i){
+		char Ci = v[i];
 		C = C_v[C_v.size() - 1];
 		sum = (sum + (C * Ci)%D)%D;
 		C_v.push_back( C * 1009 % D );
