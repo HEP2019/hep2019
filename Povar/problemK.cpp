b3cab5270953a78afd087a9baa6498f8623f4048
quantumenigma <povartetiana@gmail.com>
2019-05-16 22:28:05 +0300

    BD

--- Povar/problemK.cpp
+++ Povar/problemK.cpp
@@ -10,7 +10,7 @@ int main()
     cin>>N>>K>>B>>C>>M>>A1;
     vector <int long> mn(K);
     Ai1=A1;
-    for(int long i=2;i<=N; i++){
+    for(int long i=K-1;i>=0; i--){
         Ai=(Ai1*B+C)%M;
         Ai1=Ai;
         for(int long j=0;j<K;j++){
@@ -23,7 +23,7 @@ int main()
           }
         }
     }
-    for(int long i=K-1;i>=0;i--){
+    for(int long i=2;i<N;i++){
         cout << mn[i]<<" ";
     }
     return 0;
