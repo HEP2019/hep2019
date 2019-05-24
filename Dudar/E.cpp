6fc16525d996cc1a8e25907302e89e0d47d76bf8
FoxWise <bohdan96@gmail.com>
2019-05-24 13:40:40 +0300

    E

--- Dudar/E.cpp
+++ Dudar/E.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
+#include <vector>
 #include <map>
 
 using namespace std;
 
 int main(){
 
-    // N of serial numbers
+    // N of broken numbers
     int N;
+    vector<long long int> broken_phones;
     cin>>N;
 
-    //All serial numbers
-    map<long long int,long long int> serial_map;
-    long long int serial_number;
-    for(int i=0; i<N; i++){
-        cin>>serial_number;
-        serial_map[serial_number]++;
+    long long int broken_serial;
+    for( int i=0; i<N; i++){
+        cin>>broken_serial;
+        broken_phones.push_back(broken_serial);
     }
 
+    //All serial numbers
     //Number of broken serial numbers
     int M;
     cin>>M;
 
+    long long int serial_number;
+    map<long long int,long long int> serial_map;
     //Printing N of broken serial numbers
-    long long int broken_serial;
-    int result=0;
     for(int i=0; i<M; i++){
-        cin>>broken_serial;
-        if(serial_map[broken_serial]>0){
-            result+=serial_map[broken_serial];
-            serial_map[broken_serial]=0;
-        }
+        cin>>serial_number;    
+        serial_map[serial_number]++;    
     }
-    cout<<result<<endl;
+//
+    int s=0;
+        for(int k=0; k<broken_phones.size(); k++){
+           // for(int j=0; j<N; j++){
+                if(serial_map[broken_phones[k]]!=0) s+=serial_map[broken_phones[k]];
+             //       }
+     }
+     cout<<s<<endl;
     return 0;
 }
