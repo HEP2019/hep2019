8ea06961e2ecf3ecc1d48228691eb0c026ffb5a2
Serg <chsb2010@gmail.com>
2019-05-31 00:15:31 +0300

    K

---
+++ Chernyshenko/programs/K.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+using namespace std;
+
+int main(){
+	int N,K,B,C,M,A1,AI,Ai;
+	list<int> res(K);
+
+	cin>>N>>K>>B>>C>>M>>A1;
+	Ai=A1;
+	for(int i=0; i<N; i++){
+		AI=(Ai*B+C)%M;
+		res.push_front(Ai);
+		Ai=AI;
+	}
+	cout<<"q";
+	//res.sort();
+
+	//copy(std::next(res.begin(),N-K+1), res.end(), ostream_iterator<int>(cout," ")); 
+	}
