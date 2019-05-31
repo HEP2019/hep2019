2e52945cde2ee3a08676920e2975fb61070e68da
quantumenigma <povartetiana@gmail.com>
2019-05-31 12:15:12 +0300

    K2.0

--- Povar/problemK.cpp
+++ Povar/problemK.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <vector>
-
-
-using namespace std;
-
-int main()
-{
-    int long N,K,B,C,M,A1,Ai1,Ai,X;
+using namespace std; int main() {
+    int long B,C,M,A1,Ai1,Ai,X;
+    int N,K;
     cin>>N>>K>>B>>C>>M>>A1;
     vector <int long> mn(K);
     Ai1=A1;
-    for(int long i=2;i<N; i++){
+    for(int i=2;i<=N; i++){
         Ai=(Ai1*B+C)%M;
         Ai1=Ai;
-        for(int long j=0;j<K;j++){
-            if(Ai > mn[j])
-            {
-                X=Ai;
-                Ai=mn[j];
-                mn[j]=X;
-                
+        if(Ai >mn[K-1]){
+            for(int  j=0;j<K;j++){
+                if(Ai > mn[j]) {
+                    X=Ai;
+                    Ai=mn[j];
+                    mn[j]=X; 
+                } 
             }
         }
     }
-    for(int long i=K-1;i>=0;i--){
-        cout << mn[i]<<" ";
-    }
-    return 0;
-}
+    for(int  i=K-1;i>=0;i--){ 
+        cout << mn[i]<<" "; 
+    } 
+    return 0; }
