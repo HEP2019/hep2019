4373d1c0cfa63851f6b4599968f4b8a297b10a21
bhubad <katiaffoni@gmail.com>
2019-06-02 21:56:40 +0200

    C

--- barynova/C.cpp
+++ barynova/C.cpp
@@ -2,40 +2,35 @@
 
 using namespace std;
 
-
-main()
+int numrob(double a, double b, int k)
 {
-  int x1, y1, x2, y2, x, y, n=0;
-  double tg;
-  cin >> x1 >> y1 >> x2 >> y2 ;
-  x = x1 - x2;
-  y = y1 - y2;
+    double nexta = a /2.0;
+    double nextb = b/2.0;
+    if (((int)nexta == nexta )&& ((int)nextb == nextb)){
+        (k == 0) ? k=1 : k=k*2+1;
+       k = numrob(nexta, nextb, k);
+    }
+    if ((((int)nexta != nexta ) || ((int)nextb != nextb) ) && nexta>1 && nextb>1 ){
+        double kp;
+        (nexta>nextb) ? kp=nexta/nextb : kp=nextb/nexta;
+        if ((int)kp==kp)  (k +=(int)(kp-1)) ;
+    }
+    return k;
+}
 
-  if (x==0 or y==0)
-  {
-    n=max(abs(x),abs(y))-1;
-    if (n==-1)
-    {
-      cout<<0<<endl;
-    }
-    else
-    {
-      cout<<n<<endl;
-    }
-  }
 
-  else
-  {
-    tg = max(abs(x),abs(y))/min(abs(x),abs(y));
-  //  cout<<tg<<endl;
-    if (int(tg)-tg==0)
-    {
-      n=int(tg)-1;
-      cout<<n<<endl;
-    }
-    else
-    {
-      return 1;
-    }
-  }
+int main()
+{
+    int x1 = 0, x2 = 0, y1 = 0, y2 = 0, a = 0, b = 0, k = 0;
+    cin>> x1>>y1>>x2>>y2;
+    a = y2 - y1;
+    b = x2 - x1;
+    if(a<0)  a*=-1;
+    if(b<0)  b*=-1;
+    a == 0 ? k=b-1 : k=0;
+    b == 0 ? k=a-1 : k=k;
+    if((x1=y1) && (x2==y2) && (x1 !=x2))   x2>x1 ? k = x2-x1-1 : k=x1-x2-1;
+    if (k ==0) k = numrob(a*1.0, b*1.0, k);
+    if (k <0) k*=(-1);
+    k == -1 ? cout<<0<<endl : cout<<k<<endl;
 }
