bd0e9163409388929106173057013647fe428896
bhubad <katiaffoni@gmail.com>
2019-06-02 21:46:03 +0200

    BD

--- barynova/BD.cpp
+++ barynova/BD.cpp
@@ -1,46 +1,37 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 
-int main ()
+
+int colums(int n)
 {
-  int n, i, j, m, q=0, s=0;
-  cin>>n;
-  int arr[n+1][n+1];
-  if (n<5 or n>500)
-  {
-    return 1;
-  }
-  for (i=0; i<n; i++)
-  {
-    for (j=0; j<n; j++)
+    int totalcol = 0;
+    int array1[501][501];
+
+    for(int i = 1;i<=n;i++)
     {
-      if (i==j & (i==1 or i==0))
-      {
-        arr[i][j]=1;
-      }
-      else
-      {
-        arr[i][j]=0;
-      }
+        array1[i][1] = 1;
+        int cols = 2;
+
+
+        while (array1[i][cols]=array1[i-cols][cols]+array1[i-cols][cols-1])
+        {
+            if(i==n)
+            {
+                totalcol+=array1[i][cols];
+            }
+              ++cols;
+        }
     }
-  }
-  for (i=2;i<=n;i++)
-  {
-    for (j=1; j<i; j++)
-    {
-      s=0;
-      for (m=0; m<j; m++)
-      {
-        s+=arr[i-j][m];
-      }
-      arr[i][j]=s;
-    }
-    arr[i][i]=1;
-  }
-  for (i=1; i<n; i++)
-  {
-    q+=arr[n][i];
-  }
-  cout<<q<<endl;
+
+    return totalcol;
+}
+
+int main()
+
+{
+  int N;
+  cin >> N;
+  cout << colums(N)<<endl;
 }
