1bf43e9211a39169f5363a1e18b9369bcfc7638e
Serg <chsb2010@gmail.com>
2019-06-06 17:07:34 +0300

    K2 new

--- Chernyshenko/programs/K2.cpp
+++ Chernyshenko/programs/K2.cpp
@@ -12,16 +12,29 @@ T mynext(T iter, size_t n)
 
 int main(){
 	int N,K,B,C,M,A1,AI,Ai;
+	cin>>N>>K>>B>>C>>M>>A1;
+	//int l=K, d=0;
 	list<int> res;
 
-	cin>>N>>K>>B>>C>>M>>A1;
+	
 	Ai=A1;
 	for(int i=0; i<N; i++){
 		AI=(Ai*B+C)%M;
 		res.push_front(Ai);
+		//res.sort();
 		Ai=AI;
+		//d++;
+		/*if(d==K){
+		 res.sort();
+		 cout <<d<<endl;
+		 d=0;
+		}*/
 	}
+	
 	//cout << res.size() << ' ' << N << ' ' << K << ' ' << std::endl;
+//	cout << sizeof res << endl;
 	res.sort();
+//	cout << sizeof res << endl;
 	copy(mynext(res.begin(),N-K), res.end(), ostream_iterator<int>(cout," ")); 
+	//copy(res.begin(), res.end(), ostream_iterator<int>(cout," ")); 
 	}
